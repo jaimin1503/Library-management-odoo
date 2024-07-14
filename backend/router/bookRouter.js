5f7fb09eb33a039f7032e0f5d50768346c194ceb
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-07-14 12:14:20 +0530

    book controller bug fix

--- backend/router/bookRouter.js
+++ backend/router/bookRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 const bookRouter = express.Router();
 
-import { addBook, fatchBooks, fatchBook } from '../controllers/bookController.js';
+import { addBook, fatchBooks, fatchBook,updateBook,deleteBook } from '../controllers/bookController.js';
 
 bookRouter.route("/addBook").post(addBook);
 
 bookRouter.route("/getBooks").get(fatchBooks);
 
 bookRouter.route("/getBook/:id").get(fatchBook);
+bookRouter.route("/updateBook/:id").put(updateBook);
+bookRouter.route("/deleteBook/:id").delete(deleteBook);
 //Raj
 export { bookRouter };
