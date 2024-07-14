30e865dc3f275ba2104bd6aa8c455ccc8bdfd088
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-07-14 14:52:16 +0530

    all api is done

--- backend/models/detailsScema.js
+++ backend/models/detailsScema.js
@@ -1,6 +1,17 @@
 import mongoose from "mongoose";
 const detailSchema=new mongoose.Schema({
-   
+   dueCharge:{
+    type:Number,
+    default:100
+   },
+   dueDays:{
+    type:Number,
+    default:30
+   },
+   limitIssue:{
+    type:Number,
+    default:5
+   }
 })
 const  Detail=mongoose.model("Detail",detailSchema) 
 export default Detail
--- backend/router/bookRouter.js
+++ backend/router/bookRouter.js
@@ -2,6 +2,8 @@ import express from "express";
 const bookRouter = express.Router();
 
 import { addBook, fatchBooks, fatchBook,updateBook,deleteBook } from '../controllers/bookController.js';
+import {issueBookToUser,returnBook} from "../controllers/issueBook.js";
+import {auth,isUser } from "../middlewares/authMiddleware.js";
 
 bookRouter.route("/addBook").post(addBook);
 
@@ -10,5 +12,8 @@ bookRouter.route("/getBooks").get(fatchBooks);
 bookRouter.route("/getBook/:id").get(fatchBook);
 bookRouter.route("/updateBook/:id").put(updateBook);
 bookRouter.route("/deleteBook/:id").delete(deleteBook);
+bookRouter.route("/issueBookToUser").post(auth,isUser,issueBookToUser);
+bookRouter.route("/returnBookToUser").put(auth,isUser,returnBook);
+
 //Raj
 export { bookRouter };
