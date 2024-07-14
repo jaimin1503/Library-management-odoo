48994abad12747fc97168b81be2c0647690eeeda
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-07-14 08:26:05 +0530

    be fe init

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className=" text-5xl text-white">
+        Welcome to Library management system
+      </div>
+    </div>
+  );
+}
